083f2065b6279116ae3a1304658712cb84f8156d
acgfreshman <liumuchen1996@gmail.com>
2017-04-02 08:45:04 -0700

    pull

---
+++ js/script.js
@@ -0,0 +1,35 @@
+//initiate 
+$(document).ready(init());
+
+function init(){
+    addTooltip();  
+    emailSentPop();
+    addPost();
+    testme();
+}
+
+//add toolstips to all table rows
+function addTooltip(){
+    $('[data-toggle="tooltip"]').tooltip();   
+}
+
+//popover email sent notification modal
+function emailSentPop(){
+    $(".sentMail").click(function(){
+        $("#sentMail").modal();
+    });
+}
+
+//initiate plus button
+function addPost() {
+    $('#seller-switch-btn').on('click', function() {
+        $('#plus-btn').attr('data-target', '#postProvider');
+        console.log($('#plus-btn'));
+    })
+
+    $('#buyer-switch-btn').on('click', function() {
+        $('#plus-btn').attr('data-target', '#postDemander');
+    })
+}
+
+
